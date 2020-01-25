e3011165ae64d5d9597165638cdd3f93692a9e15
54mch4n <54mch4n.1111@gmail.com>
2020-01-26 02:59:32 +0900

    refactoring

--- example/main.go
+++ example/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/54mch4n/cont"
+
+	. "github.com/54mch4n/cont"
 )
 
 func main() {
@@ -11,14 +12,18 @@ func main() {
 		ex2 = []int{1, 2, 3}
 		ex3 = "123"
 	)
+
 	fmt.Println(ex1, ex2, ex3)
-	if cont.Contains(ex1, "1") {
+
+	if Contains(ex1, "1") {
 		fmt.Println("ex1: OK!")
 	}
-	if cont.Contains(ex2, 1) {
+
+	if Contains(ex2, 1) {
 		fmt.Println("ex2: OK!")
 	}
-	if cont.Contains(ex3, "1") {
+
+	if Contains(ex3, "1") {
 		fmt.Println("ex3: OK!")
 	}
 }
