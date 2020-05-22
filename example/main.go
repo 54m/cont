e465bdaa6ed20ee7d09a53bb4730d6165deac8fc
54mch4n <54mch4n.1111@gmail.com>
2020-05-22 19:09:23 +0900

    upgrade: Release

--- example/main.go
+++ example/main.go
@@ -1,39 +1,65 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"time"
 
 	"github.com/54mch4n/cont"
 )
 
 func main() {
-	var (
-		now = time.Now()
-		ex1 = []string{"1", "2", "3"}
-		ex2 = []int{1, 2, 3}
-		ex3 = "123"
-		ex4 = map[string]interface{}{"a": "1", "b": "2", "c": "3", "d": now}
-	)
+	ExampleContainsStringSlice()
+	ExampleContainsIntSlice()
+	ExampleContainsString()
+	ExampleContainsMap()
+}
 
-	log.Println(ex1)
-	log.Println(ex2)
-	log.Println(ex3)
-	log.Println(ex4)
-
-	if cont.Contains(ex1, "1") {
-		log.Println("ex1: OK!")
+func ExampleContainsStringSlice() {
+	contents := []string{"1", "2", "3"}
+	fmt.Printf("%#v\n", contents)
+	if cont.Contains(contents, "1") {
+		fmt.Println("StringSlice: OK!")
 	}
+	// Output: []string{"1", "2", "3"}
+	// Output: StringSlice: OK!
+}
 
-	if cont.Contains(ex2, 1) {
-		log.Println("ex2: OK!")
+func ExampleContainsIntSlice() {
+	contents := []int{1, 2, 3}
+	fmt.Printf("%#v\n", contents)
+	if cont.Contains(contents, 1) {
+		fmt.Println("IntSlice: OK!")
 	}
+	// Output: []int{1, 2, 3}
+	// Output: IntSlice: OK!
+}
 
-	if cont.Contains(ex3, "1") {
-		log.Println("ex3: OK!")
+func ExampleContainsString() {
+	contents := "123"
+	fmt.Printf("%#v\n", contents)
+	if cont.Contains(contents, "1") {
+		fmt.Println("String: OK!")
 	}
+	// Output: "123"
+	// Output: String: OK!
+}
 
-	if cont.Contains(ex4, now) {
-		log.Println("ex4: OK!")
+func ExampleContainsMap() {
+	now := time.Now()
+	contents := map[string]interface{}{"a": "1", "b": 2, "c": "3", "d": now}
+	fmt.Printf("%#v\n", contents)
+	if cont.Contains(contents, now) {
+		fmt.Println("Map: OK! - time.Time")
 	}
+	if cont.Contains(contents, 2) {
+		fmt.Println("Map: OK! - int")
+	}
+	if cont.Contains(contents, "c") {
+		fmt.Println("Map: OK! - string")
+	}
+	// nolint:lll
+	// Output: map[string]interface {}{"a":"1", "b":2, "c":"3", "d":time.Time{wall:0x1234567890, ext:xxx, loc:(*time.Location)(0x123456789)}}
+	// Output: Map: OK! - time.Time
+	// Output: Map: OK! - int
+	// Output: Map: OK! - string
 }
